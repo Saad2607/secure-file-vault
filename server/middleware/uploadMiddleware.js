b3bc215ac51320d39560e2ecd671f6d0faafb220
Saad Shaikh <msaadbjs7@gmail.com>
2026-04-08 23:48:46 +0530

    Initial Commit

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -1,22 +1,24 @@
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("../config/cloudinary");
+const path = require("path");
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
-    params: async (req, file) => {
+    params: (req, file) => {
         const ext = path.extname(file.originalname).toLowerCase();
+        const name = path.parse(file.originalname).name;
 
         let resourceType = "image";
 
         if (ext === ".pdf" || ext === ".doc" || ext === ".docx") {
-            resourceType = "raw";
+            resourceType = "raw"; // ✅ FIX
         }
 
         return {
             folder: "secure-file-vault",
             resource_type: resourceType,
-            public_id: Date.now() + "-" + path.parse(file.originalname).name,
+            public_id: Date.now() + "-" + name,
         };
     },
 });
