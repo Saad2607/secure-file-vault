f6392993761d793ac784844edd193a58af22b2a9
Saad Shaikh <msaadbjs7@gmail.com>
2026-04-09 16:24:52 +0530

    Initial Commit

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -7,21 +7,14 @@ const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: (req, file) => {
         const path = require("path");
-        const ext = path.extname(file.originalname).toLowerCase();
         const name = path.parse(file.originalname).name;
-
-        let resourceType = "image";
-
-        if (ext === ".pdf" || ext === ".doc" || ext === ".docx") {
-            resourceType = "raw";
-        }
+        const ext = path.extname(file.originalname).replace(".", "");
 
         return {
             folder: "secure-file-vault",
-            resource_type: resourceType,
-            type: "upload", // ✅ VERY IMPORTANT FIX
+            resource_type: "image", // 🔥 FORCE IMAGE
             public_id: Date.now() + "-" + name,
-            format: ext.replace(".", ""),
+            format: ext, // keeps pdf extension
         };
     },
 });
